b36f32e2cc18cf60cac8c1c64a968f6ac66bfa01
vikas0701 <vikas.dandge07@gmail.com>
2022-01-25 23:39:23 +0530

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookWebApp.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerPassword { get; set; }
+    }
+}
